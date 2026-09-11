5af6635f4ea23496640f91cd57c300fbfd91c885
criselcrackoff <criselcrack.off@gmail.com>
2026-09-11 19:41:03 -0300

    fix(game): prevent crashes from out-of-range values

--- scripts/power.py
+++ scripts/power.py
@@ -13,19 +13,21 @@ class PowerScript(Script):
         }
 
     def update(self, dt):
-
-        self.game.usage = max(1, min(6, self.game.usage))
-        usage = self.game.usage
-        progress = self.game.get_image("PowerProgress")
-        self.power -= dt * (usage / 10)
-        if self.power <= 0:
-            self.game.blackout = True
-            self.power = 0
-        if self.power < 10:
-            if int(self.game.texts[3].get_x()) != 82:
-                self.game.texts[3].set_x(82)
-        else:
-            if int(self.game.texts[3].get_x()) != 67:
-                self.game.texts[3].set_x(67)
-        progress.change_image(f"./assets/sprites/Mechanics/PowerUsage/power_usage_progression_{usage}.png")
-        self.game.texts[3].set_text(str(int(self.power)))
+        try:
+            self.game.usage = max(1, min(6, self.game.usage))
+            usage = self.game.usage
+            progress = self.game.get_image("PowerProgress")
+            self.power -= dt * (usage / 10)
+            if self.power <= 0:
+                self.game.blackout = True
+                self.power = 0
+            if self.power < 10:
+                if int(self.game.texts[3].get_x()) != 82:
+                    self.game.texts[3].set_x(82)
+            else:
+                if int(self.game.texts[3].get_x()) != 67:
+                    self.game.texts[3].set_x(67)
+            progress.change_image(f"./assets/sprites/Mechanics/PowerUsage/power_usage_progression_{usage}.png")
+            self.game.texts[3].set_text(str(int(self.power)))
+        except:
+            return "list index out of range"            
